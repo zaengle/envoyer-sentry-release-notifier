29f51142d2b368d429908631db477925240dff46
Jesse Schutt <jesse@zaengle.com>
2020-07-10 15:23:41 -0500

    Changed commit hash name

--- src/EnvoyerSentryReleaseNotifier.php
+++ src/EnvoyerSentryReleaseNotifier.php
@@ -12,7 +12,7 @@ class EnvoyerSentryReleaseNotifier {
      */
     public function getCommitHash(): string
     {
-        $latestCommitHash = base_path('.latest_commit_hash');
+        $latestCommitHash = base_path('.commit_hash');
 
         if (file_exists($latestCommitHash)) {
             return trim(exec(sprintf('cat %s', $latestCommitHash)));
